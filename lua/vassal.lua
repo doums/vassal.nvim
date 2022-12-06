5b13ac274d65649ccfbc178e7e293aeea1bf1301
pierre <dommerc.pierre@gmail.com>
2022-12-06 23:38:50 +0100

    feat: use vim.notify

--- lua/vassal.lua
+++ lua/vassal.lua
@@ -3,17 +3,18 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/. ]]
 
 local fn = vim.fn
-local api = vim.api
+local lvl = vim.log.levels
 
 local jobs = {}
 local _commands = {}
 
 local function on_exit(id, code)
   if code == 0 then
-    print(string.format('✓ [%s]', jobs[id]))
+    vim.notify(string.format('✓ [%s]', jobs[id]), lvl.INFO)
   else
-    api.nvim_err_writeln(
-      string.format('✕ [%s] command failed, exit code [%d]', jobs[id], code)
+    vim.notify(
+      string.format('✕ [%s] command failed, exit code [%d]', jobs[id], code),
+      lvl.ERROR
     )
   end
   jobs[id] = nil
@@ -26,13 +27,13 @@ local options = {
 
 local function launch()
   for _, command in ipairs(_commands) do
-    print(string.format('running [%s]', command))
+    vim.notify(string.format('running [%s]', command), lvl.INFO)
     local job_id = fn.jobstart(command, options)
     if job_id == 0 then
-      api.nvim_err_writeln('[vassal] invalid argument')
+      vim.notify('[vassal] invalid argument', lvl.ERROR)
       return
     elseif job_id == -1 then
-      api.nvim_err_writeln('[vassal] command or shell is not executable')
+      vim.notify('[vassal] command or shell is not executable', lvl.ERROR)
       return
     end
     jobs[job_id] = command
