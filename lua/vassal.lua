0ad365daf3976bf852d6dd75dbcdb71b046bf69c
pierre <dommerc.pierre@gmail.com>
2024-09-15 01:50:15 +0200

    rewrite using vim.system

--- lua/vassal.lua
+++ lua/vassal.lua
@@ -2,41 +2,31 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-local fn = vim.fn
 local lvl = vim.log.levels
-
-local jobs = {}
 local _commands = {}
 
-local function on_exit(id, code)
-  if code == 0 then
-    vim.notify(string.format('✓ [%s]', jobs[id]), lvl.INFO)
+local function on_exit(c)
+  if c.code == 0 then
+    print(c.stdout)
+    print(' ✓')
   else
-    vim.notify(
-      string.format('✕ [%s] command failed, exit code [%d]', jobs[id], code),
-      lvl.ERROR
-    )
+    print(c.stderr)
+    print(string.format(' ✕ command failed, exit code [%d]', c.code))
   end
-  jobs[id] = nil
 end
-
-local options = {
-  detach = true,
-  on_exit = on_exit,
-}
 
 local function launch()
   for _, command in ipairs(_commands) do
-    vim.notify(string.format('… running [%s]', command), lvl.INFO)
-    local job_id = fn.jobstart(command, options)
-    if job_id == 0 then
-      vim.notify('[vassal] invalid argument', lvl.ERROR)
-      return
-    elseif job_id == -1 then
-      vim.notify('[vassal] command or shell is not executable', lvl.ERROR)
-      return
+    if type(command) == 'string' then
+      command = vim.split(command, '%s', { trimempty = true })
     end
-    jobs[job_id] = command
+    local cmd_str = table.concat(command, ' ')
+    local s = pcall(vim.system, command, { text = true }, on_exit)
+    if s then
+      vim.notify(string.format(' … running [%s]', cmd_str), lvl.INFO)
+    else
+      vim.notify(string.format(' ✕ invalid command [%s]', cmd_str), lvl.ERROR)
+    end
   end
 end
 
